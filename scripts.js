7aa8b6cf03fc664ff5a426ad33a331ae53cc89b3
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-25 16:25:27 -0300

    Implement: onclick script event

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-document
-  .querySelector("button.fat")
-  .addEventListener("click", function(){
-    document
-      .querySelector("#footer")
-      .classList
-      .toggle("hide")
-  })
+function onOff(){
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("hide")
+}
+
