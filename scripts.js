185f8b7510b01fdb542b5baa6f6a00396466e6c1
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-26 02:38:13 -0300

    Implement: function for buttons on ideas page

--- scripts.js
+++ scripts.js
@@ -3,5 +3,15 @@ function onOff(){
     .querySelector("#modal")
     .classList
     .toggle("hide")
+
+  document
+  .querySelector("body")
+  .classList
+  .toggle("hideScroll")
+
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
 }
 
