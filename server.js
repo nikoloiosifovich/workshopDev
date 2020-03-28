a58a1a0bc825090b521c7a47cd05a49ae94b75bd
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-28 14:37:09 -0300

    implement: extends ideas array values

--- server.js
+++ server.js
@@ -42,10 +42,34 @@ const ideas = [
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
     url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+    title: "Meditação",
+    category: "Mentalidade",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaokê",
+    category: "Diversão em Família",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaokê",
+    category: "Diversão em Família",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
   }
 ];
 
-const lastThreeIdeas = ideas.reverse().slice(0, 3);
+// const lastThreeIdeas = ideas.reverse().slice(0, 3);
 
 // configurar arquivos estáticos (css, scripts, imagnes)
 server.use(express.static("public"));
@@ -60,11 +84,16 @@ nunjuks.configure("views", {
 // criei uma rota /
 // e capturo o projeto do cliente para responder
 server.get("/", function(req, res) {
+  const lastThreeIdeas = [...ideas].reverse().slice(0, 3);
+
   return res.render("index.html", { ideas: lastThreeIdeas });
 });
 
 server.get("/ideias", function(req, res) {
-  return res.render("ideias.html", { ideas });
+  // estou espalhado o conteudo de ideas, não mais a referência a ele.
+  const allIdeas = [...ideas].reverse();
+
+  return res.render("ideias.html", { ideas: allIdeas });
 });
 
 // liguei meu servidor na porta 3333
