1e2bcc0a9f6c1d5dd6f50c0996d61764a1c0236a
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-28 14:11:35 -0300

    implement: last 3 ideas for home page

--- server.js
+++ server.js
@@ -26,8 +26,18 @@ const ideas = [
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
     url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaokê",
+    category: "Diversão em Família",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
   }
 ];
+
+const lastThreeIdeas = ideas.reverse().splice(0, 3);
 
 // configurar arquivos estáticos (css, scripts, imagnes)
 server.use(express.static("public"));
@@ -42,7 +52,7 @@ nunjuks.configure("views", {
 // criei uma rota /
 // e capturo o projeto do cliente para responder
 server.get("/", function(req, res) {
-  return res.render("index.html", { ideas });
+  return res.render("index.html", { ideas: lastThreeIdeas });
 });
 
 server.get("/ideias", function(req, res) {
