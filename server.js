0285ff56be5c9ffebf05f07b687962dc7dbffd1c
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-28 11:43:17 -0300

    setting: basic configuration for nunjuks

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// usei o express para criar e configurar meu servidor
 const express = require("express");
 const server = express();
 
+// configurar arquivos estáticos (css, scripts, imagnes)
 server.use(express.static("public"));
 
+// configuração do nunjuks (template builder. eg. jinja)
+const nunjuks = require("nunjucks");
+nunjuks.configure("views", {
+  express: server
+});
+
+// criei uma rota /
+// e capturo o projeto do cliente para responder
 server.get("/", function(req, res) {
-  console.log("RESTARTING...\n" + __dirname + __filename);
-  return res.sendFile(__dirname + "/index.html");
+  return res.render("index.html");
 });
 
 server.get("/ideias", function(req, res) {
-  return res.sendFile(__dirname + "/ideias.html");
+  return res.render("ideias.html");
 });
 
+// liguei meu servidor na porta 3333
 server.listen(3333);
