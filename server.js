f4e1b8d9f7d66ab219826db21f6e1f3f3bb28a88
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-28 12:41:28 -0300

    implement: for in hmtl with data from nunjuks

--- server.js
+++ server.js
@@ -2,6 +2,33 @@
 const express = require("express");
 const server = express();
 
+const ideas = [
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Cursos de Programação",
+    category: "Estudo",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+    title: "Exercícios",
+    category: "Saúde",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+    title: "Meditação",
+    category: "Mentalidade",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto labore ratione iste quidem ips deserunt, voluptas nemo distinctio totam quia sunt dolores asperiores, in cumque autem dolorem, animi obcaecati repudiandae.",
+    url: "http://udemy.com"
+  }
+];
+
 // configurar arquivos estáticos (css, scripts, imagnes)
 server.use(express.static("public"));
 
@@ -15,9 +42,7 @@ nunjuks.configure("views", {
 // criei uma rota /
 // e capturo o projeto do cliente para responder
 server.get("/", function(req, res) {
-  // exemplo de utilização do nunjuks
-  const h1 = "OI DO BACKEND";
-  return res.render("index.html", { title: h1 });
+  return res.render("index.html", { ideas });
 });
 
 server.get("/ideias", function(req, res) {
