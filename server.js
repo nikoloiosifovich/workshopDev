e6fdb1d1e2251c890cf2d0273391b9ce834a6642
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-28 11:08:50 -0300

    setting: configure routes for express

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const server = express();
 
+server.use(express.static("public"));
+
 server.get("/", function(req, res) {
-  console.log("FULL");
-  return res.send("I'm here!");
+  console.log("RESTARTING...\n" + __dirname + __filename);
+  return res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000);
+server.get("/ideias", function(req, res) {
+  return res.sendFile(__dirname + "/ideias.html");
+});
+
+server.listen(3333);
